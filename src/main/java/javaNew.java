ae4479523a22bf93509daa43f953000beb943175
Sangid11 <sangeeta0309@yahoo.com>
2023-03-20 18:27:15 -0400

    Second

---
+++ src/main/java/javaNew.java
@@ -0,0 +1,7 @@
+public class javaNew {
+    public static void main(String[] args) {
+        String txt = "My new Git practice";
+        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(txt.toLowerCase());   // Outputs "hello world"
+    }
+}
